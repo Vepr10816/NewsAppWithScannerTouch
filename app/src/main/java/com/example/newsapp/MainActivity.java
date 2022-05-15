c86565bd51979bc2b77665fa56a42c0def2e6212
Vepr <isip_a.n.veprickiy@mpt.ru>
2022-05-15 15:24:37 +0300

    Добавлен сканер отпечатка пальца

--- app/src/main/java/com/example/newsapp/MainActivity.java
+++ app/src/main/java/com/example/newsapp/MainActivity.java
@@ -1,22 +1,70 @@
 package com.example.newsapp;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.database.Cursor;
+//import android.hardware.biometrics.BiometricPrompt;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricPrompt;
+import androidx.core.content.ContextCompat;
+
+import java.util.concurrent.Executor;
+
 public class MainActivity extends AppCompatActivity {
     EditText Login, Password;
-    Button SignIn,Registration;
+    Button SignIn,Registration,btnScan;
     DatabaseHelper databaseHelper;
+    BiometricPrompt biometricPrompt;
+    Executor executor;
+    BiometricPrompt.PromptInfo promptInfo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnScan = findViewById(R.id.btnTouch);
+        executor = ContextCompat.getMainExecutor(this);
+        biometricPrompt = new BiometricPrompt(MainActivity.this,
+                executor,new BiometricPrompt.AuthenticationCallback(){
+            @Override
+            public void onAuthenticationError(int errorCode,
+                                              @NonNull CharSequence errString){
+                super.onAuthenticationError(errorCode,errString);
+                Log.e("ErrAuth",errString.toString());
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result){
+                super.onAuthenticationSucceeded(result);
+                Toast.makeText(getApplicationContext(), "Успешно!", Toast.LENGTH_LONG).show();
+                Intent mainIntent = new Intent(MainActivity.this, AdminPage.class);
+                mainIntent.putExtra("user", "2");
+                MainActivity.this.startActivity(mainIntent);
+                MainActivity.this.finish();
+            }
+
+            @Override
+            public void onAuthenticationFailed(){
+                super.onAuthenticationFailed();
+                Log.e("FailedAUTH","Fail");
+            }
+        });
+        promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Авторизация в новостное приложение")
+                .setSubtitle("Прислоните палец")
+                .setNegativeButtonText("Отмена")
+                .build();
+
+        btnScan.setOnClickListener(view -> {
+            biometricPrompt.authenticate(promptInfo);
+        });
+
         Login = findViewById(R.id.et_email);
         Password = findViewById(R.id.et_password);
         SignIn = findViewById(R.id.sign_in);
